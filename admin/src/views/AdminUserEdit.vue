3c498f2240612325273e93d864526be626d8807f
koyomi <1936002457@qq.com>
2019-07-08 13:34:03 +0800

    2-16

--- admin/src/views/AdminUserEdit.vue
+++ admin/src/views/AdminUserEdit.vue
@@ -2,14 +2,12 @@
     <div>
         <h1>{{id? '编辑':'新建'}}管理员</h1>
         <el-form label-width="120px" @submit.native.prevent="save">
-            <el-form-item label="上级分类">
-                <el-select v-model="model.parent" placeholder="请选择">
-                    <el-option v-for="item in parents" :key="item._id" 
-                    :label="item.name" :value="item._id"></el-option>
-                </el-select>
-            </el-form-item>
+            
             <el-form-item label="名称">
                 <el-input v-model="model.name" width="50px"></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+                <el-input v-model="model.password" width="50px" type="password"></el-input>
             </el-form-item>
             <el-form-item>
                 <el-button type="primary" native-type="submit">保存</el-button>
@@ -25,35 +23,29 @@ export default {
     },
     data(){
         return {
-            model: {},
-            parents: {}
+            model: {}
         }
     },
     methods: {
         async save(){
             let res
             if(this.id){
-                res = await this.$http.put('/rest/categories/'+this.id,this.model)
+                res = await this.$http.put('/rest/admin_users/'+this.id,this.model)
             }else{
-                res = await this.$http.post('/rest/categories/',this.model)
+                res = await this.$http.post('/rest/admin_users/',this.model)
             }
-            this.$router.push('/categories/list')
+            this.$router.push('/admin_users/list')
             this.$message({
                 type: 'success',
                 message: '保存成功'
             })
         },
         async fetch(){
-            const res=await this.$http.get('/rest/categories/'+this.id)
+            const res=await this.$http.get('/rest/admin_users/'+this.id)
             this.model=res.data
-        },
-        async fetchParents(){
-            const res=await this.$http.get('/rest/categories')
-            this.parents=res.data
         }
     },
     created(){
-        this.fetchParents()
         this.id && this.fetch()
     }
 }
--- admin/src/views/AdminUserList.vue
+++ admin/src/views/AdminUserList.vue
@@ -3,11 +3,11 @@
     <h1>管理员列表</h1>
     <el-table :data="items">
       <el-table-column prop="_id" label="ID"></el-table-column>
-      <el-table-column prop="parent.name" label="上级ID"></el-table-column>
-      <el-table-column prop="name" label="分类名称"></el-table-column>
+      <el-table-column prop="name" label="管理员名称"></el-table-column>
+      <el-table-column prop="password" label="密码"></el-table-column>
       <el-table-column label="操作">
         <template slot-scope="scope">
-          <el-button type="text" size="small" @click="$router.push('/categories/edit/'+scope.row._id)"
+          <el-button type="text" size="small" @click="$router.push('/admin_users/edit/'+scope.row._id)"
           >编辑</el-button>
           <el-button type="text" size="small" @click="remove(scope.row)">删除</el-button>
         </template>
@@ -28,7 +28,7 @@ export default {
   },
   methods: {
     async fetch() {
-      const res = await this.$http.get("/rest/categories");
+      const res = await this.$http.get("/rest/admin_users");
       this.items = res.data;
     },
     async remove(row) {
@@ -38,7 +38,7 @@ export default {
         type: "warning"
       })
         .then(async () => {
-          const res = await this.$http.delete("/rest/categories/" + row._id);
+          const res = await this.$http.delete("/rest/admin_users/" + row._id);
           this.$message({
             type: "success",
             message: "删除成功!"
@@ -56,5 +56,3 @@ export default {
 };
 </script>
 
-<style>
-</style>
